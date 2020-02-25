c895798385ad902935508903a26126a38c1a4a9b
Martijn Comans <m.r.comans@student.tudelft.nl>
2020-02-25 12:54:11 +0100

    Add initial project

---
+++ src/main/scala/nl/tudelft/fruitarian/Main.scala
@@ -0,0 +1,9 @@
+package nl.tudelft.fruitarian
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    FruitarianServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
