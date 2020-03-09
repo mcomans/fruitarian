f36c967a947fe1c832e9f6182cc57d6cd9736a0f
Nick Belzer <me@nickbelzer.me>
2020-03-09 19:07:04 +0100

    Remove default project

--- src/main/scala/nl/tudelft/Main.scala
+++
@@ -1,9 +0,0 @@
-package nl.tudelft
-
-import akka.actor.typed.ActorSystem
-import nl.tudelft.GreeterMain.SayHello
-
-object Main extends App {
-  val greeterMain: ActorSystem[GreeterMain.SayHello] = ActorSystem(GreeterMain(), "AkkaQuickStart")
-  greeterMain ! SayHello("Charles")
-}
