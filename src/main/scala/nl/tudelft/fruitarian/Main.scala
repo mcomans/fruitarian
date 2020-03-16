dacaf4a34114fc4dd3d044b45460aeabc6db9251
Nick Belzer <me@nickbelzer.me>
2020-03-16 20:58:47 +0100

    Move BasicLogger to BasicLogger class

--- src/main/scala/nl/tudelft/fruitarian/Main.scala
+++ src/main/scala/nl/tudelft/fruitarian/Main.scala
@@ -1,22 +1,9 @@
 package nl.tudelft.fruitarian
 
-import nl.tudelft.fruitarian.p2p.{Address, Msg, MsgHeader, MsgType,
-  TCPHandler}
-import nl.tudelft.fruitarian.patterns.Observer
+import nl.tudelft.fruitarian.p2p.{Address, Msg, MsgHeader, MsgType, TCPHandler}
 
 object Main extends App {
   val handler = new TCPHandler()
-
-  object BasicLogger extends Observer[Msg] {
-    def receiveUpdate(event: Msg): Unit = event match {
-      case Msg(MsgHeader(msgType, from, _), text) if msgType == MsgType.Text =>
-        val msg = text.map(_.toChar).mkString
-        println(s"[${from.socket}][TEXT]: $msg")
-      case Msg(MsgHeader(msgType, from, _), body) if msgType == MsgType.Bytes =>
-        println(s"[${from.socket}][BYTES]: $body")
-    }
-  }
-
   handler.addMessageObserver(BasicLogger)
 
   val helloWorldMessage = Msg(MsgHeader(
