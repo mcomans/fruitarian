19b09365b6c3fc54056f95a5fa1a92c3cf381564
Daan Oudejans <D.A.J.Oudejans@student.tudelft.nl>
2020-03-18 21:54:12 +0100

    Cleanup

--- run.py
+++ run.py
@@ -5,28 +5,35 @@ import subprocess
 import sys
 import time
 import queue
-from threading import Thread
+import threading
 
 PATH_TO_BINARY = './target/universal/stage/bin/fruitarian'
 FIRST_PORT = 5000
 
 procs = []
-writequeue = queue.Queue()
-threads = []
 
 
 def log(str):
+    """Log str immediately to stdout"""
     sys.stdout.write(str)
     sys.stdout.flush()
 
 
 def enqueue_output(out, idx, queue):
+    """
+    Enqueue some output in order to be written in order later on.
+
+    out: output (i.e. of stdout of a process)
+    idx: internal index of the node/process
+    queue: the queue to enqueue to
+    """
     for line in iter(out.readline, b''):
         queue.put(f"[{idx}] | {line}")
     out.close()
 
 
 def package():
+    """Runs `sbt clean stage` and waits for it to finish."""
     log("-- Running sbt clean stage\n")
     proc = subprocess.Popen(['sbt', 'clean', 'stage'], stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT, universal_newlines=True)
@@ -36,31 +43,41 @@ def package():
 
 
 def start_first_node():
-    log("-- Starting first fruitarian node 0\n")
+    """Start the first node of the fruitarian network without any parameters."""
+    log("-- Starting fruitarian node 0\n")
     proc = subprocess.Popen(['./target/universal/stage/bin/fruitarian'], stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT, universal_newlines=True)
-    return proc
+    procs.append(proc)
 
 
-def add_node(nr, server_port, known_port):
-    log(f"-- Starting fruitarian node {nr}\n")
+def add_node(idx, server_port, known_port):
+    """
+    Add a node to the fruitarian network.
+
+    idx: the current node index (only for reference within this script and logs)
+    server_port: the port at which to start the server at this node
+    known_port: the port of an already known node
+    """
+    log(f"-- Starting fruitarian node {idx}\n")
     proc = subprocess.Popen(['./target/universal/stage/bin/fruitarian', str(server_port), 'localhost', str(known_port)],
                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    return proc
+    procs.append(proc)
 
 
 def parse_args():
+    """Parse the arguments of the script"""
     parser = argparse.ArgumentParser(
         description="Run the Fruitarian project")
 
     parser.add_argument('-n', '--nodes', type=int, required=True,
                         help="number of nodes to start")
-    parser.add_argument('-s', '--skip', action='store_true',
+    parser.add_argument('-s', '--skip-packaging', action='store_true',
                         help="skip packaging of the Scala project")
     return parser.parse_args()
 
 
 def stop(sig, frame):
+    """Gracefully stop all the processes"""
     print("-- Stopping...")
     for proc in procs:
         proc.terminate()
@@ -69,31 +86,33 @@ def stop(sig, frame):
 
 def main():
     args = parse_args()
+    # Register handler for SIGINT
     signal.signal(signal.SIGINT, stop)
 
-    if not args.skip:
+    # Package the project using sbt
+    if not args.skip_packaging:
         package()
-
     if args.nodes < 1:
         sys.exit("At least one node should be started")
 
-    first_node = start_first_node()
-    procs.append(first_node)
-    time.sleep(1)
-
+    # Start a first node and add any additional nodes
+    start_first_node()
     for i in range(1, args.nodes):
-        proc = add_node(i, FIRST_PORT + i, FIRST_PORT + i - 1)
-        procs.append(proc)
+        # Make sure the nodes have some time to start
         time.sleep(1)
+        add_node(i, FIRST_PORT + i, FIRST_PORT + i - 1)
 
+    # Start threads and a queue for queueing the outputs of the processes/nodes
+    threads = []
+    writequeue = queue.Queue()
     for idx, p in enumerate(procs):
-        threads.append(Thread(target=enqueue_output,
-                              args=(p.stdout, idx, writequeue)))
-
+        threads.append(threading.Thread(target=enqueue_output,
+                                        args=(p.stdout, idx, writequeue)))
     for t in threads:
         t.daemon = True
         t.start()
 
+    # Continuously check the queue for any new output and log it
     while True:
         try:
             line = writequeue.get_nowait()
@@ -104,7 +123,7 @@ def main():
         else:
             log(line)
 
-        if all(p.poll() is not None for p in procs):
+        if all(proc.poll() is not None for proc in procs):
             break
 
     log("All nodes stopped")
