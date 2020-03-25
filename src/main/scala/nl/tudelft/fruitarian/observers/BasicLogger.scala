596ba839d3d960083049cf93cbf7e5c449c65eff
Nick Belzer <me@nickbelzer.me>
2020-03-25 19:26:33 +0100

    Implement custom logging for TransmitRequest and TransmitMessage

--- src/main/scala/nl/tudelft/fruitarian/observers/BasicLogger.scala
+++ src/main/scala/nl/tudelft/fruitarian/observers/BasicLogger.scala
@@ -1,12 +1,14 @@
 package nl.tudelft.fruitarian.observers
 
-import nl.tudelft.fruitarian.p2p.messages.{FruitarianMessage, TextMessage}
+import nl.tudelft.fruitarian.p2p.messages.{FruitarianMessage, TextMessage, TransmitMessage, TransmitRequest}
 import nl.tudelft.fruitarian.patterns.Observer
 
 /* Example Observer that logs all incoming messages. */
 object BasicLogger extends Observer[FruitarianMessage] {
   def receiveUpdate(event: FruitarianMessage): Unit = event match {
     case TextMessage(from, _, message) => println(s"[${from.socket}][TEXT]: $message")
+    case TransmitRequest(from, _) => println(s"[${from.socket}][MESSAGE_REQUEST]")
+    case TransmitMessage(from, _, message) => println(s"[${from.socket}][BYTE][${message.length}]: 0x${message.map("%02X" format _).mkString.take(40)}...[truncated]")
     case _ => println(event)
   }
 }
