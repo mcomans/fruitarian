035fef54d01d8c0228b9b8ba971a820ed27e817b
Nick Belzer <me@nickbelzer.me>
2020-03-25 14:36:18 +0100

    Refactor Main logic to use a simple isCentreNode flag

--- src/main/scala/nl/tudelft/fruitarian/Main.scala
+++ src/main/scala/nl/tudelft/fruitarian/Main.scala
@@ -1,16 +1,21 @@
 package nl.tudelft.fruitarian
 
+import java.net.InetSocketAddress
+
 import nl.tudelft.fruitarian.models.NetworkInfo
 import nl.tudelft.fruitarian.observers.{BasicLogger, EntryObserver, Greeter, TransmissionObserver}
+import nl.tudelft.fruitarian.p2p.messages.EntryRequest
 import nl.tudelft.fruitarian.p2p.{Address, TCPHandler}
 
 object Main extends App {
   /* This example will start a Transmission Message Round with itself. */
 
-  //TODO: remove when center node election is implemented.
-  val networkInfo = if (args.length > 0) new NetworkInfo(false) else new NetworkInfo(true)
+  val isCentreNode: Boolean = args.length == 0
 
-  val handler = if (args.length > 0) new TCPHandler(args(0).toInt) else new TCPHandler()
+  //TODO: remove when center node election is implemented.
+  val networkInfo = new NetworkInfo(isCentreNode)
+
+  val handler = if (isCentreNode) new TCPHandler() else new TCPHandler(args(0).toInt)
   networkInfo.ownAddress = Address(handler.serverHost)
   handler.addMessageObserver(BasicLogger)
   handler.addMessageObserver(new Greeter(handler))
@@ -20,11 +25,21 @@ object Main extends App {
 
   Thread.sleep(1000)
 
-  transmissionObserver.queueMessage("Hi there")
+  transmissionObserver.queueMessage(s"Hi there from ${networkInfo.ownAddress.socket.getPort}")
 
-  if (args.length == 0) {
+  // If we are a the centre node.
+  if (isCentreNode) {
     // TODO: Actually start a message round once elected centre node.
     transmissionObserver.startMessageRound()
+  }
+
+  // If we are a client node.
+  if (!isCentreNode) {
+    val helloWorldMessage = EntryRequest(
+      Address(handler.serverHost),
+      Address(new InetSocketAddress(args(1), args(2).toInt)))
+
+    handler.sendMessage(helloWorldMessage)
   }
 
   sys.addShutdownHook(() => {
