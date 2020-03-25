7644360ef2fb8b9aa536b6faf9b2477bb20c8312
Nick Belzer <me@nickbelzer.me>
2020-03-25 19:48:52 +0100

    Change TransmitMessage log format

--- src/main/scala/nl/tudelft/fruitarian/observers/BasicLogger.scala
+++ src/main/scala/nl/tudelft/fruitarian/observers/BasicLogger.scala
@@ -8,7 +8,7 @@ object BasicLogger extends Observer[FruitarianMessage] {
   def receiveUpdate(event: FruitarianMessage): Unit = event match {
     case TextMessage(from, _, message) => println(s"[${from.socket}][TEXT]: $message")
     case TransmitRequest(from, _) => println(s"[${from.socket}][MESSAGE_REQUEST]")
-    case TransmitMessage(from, _, message) => println(s"[${from.socket}][BYTE][${message.length}]: 0x${message.map("%02X" format _).mkString.take(40)}...[truncated]")
+    case TransmitMessage(from, _, message) => println(s"[${from.socket}][BYTE][${message.length}]: ${message.map("%02X" format _).mkString("utf-8").take(40)}...[truncated]")
     case _ => println(event)
   }
 }
