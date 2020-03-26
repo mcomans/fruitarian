047795a7d2471d8ec2112684d6d54ac147c91db6
Nick Belzer <me@nickbelzer.me>
2020-03-26 13:16:55 +0100

    Fix incorrect hex logging

--- src/main/scala/nl/tudelft/fruitarian/observers/BasicLogger.scala
+++ src/main/scala/nl/tudelft/fruitarian/observers/BasicLogger.scala
@@ -8,7 +8,7 @@ object BasicLogger extends Observer[FruitarianMessage] {
   def receiveUpdate(event: FruitarianMessage): Unit = event match {
     case TextMessage(from, _, message) => println(s"[${from.socket}][TEXT]: $message")
     case TransmitRequest(from, _, roundId) => println(s"[${from.socket}][R$roundId][MESSAGE_REQUEST]")
-    case TransmitMessage(from, _, (roundId, message)) => println(s"[${from.socket}][R$roundId][BYTE][${message.length}]: ${message.map("%02X" format _).mkString("utf-8").take(40)}...[truncated]")
+    case TransmitMessage(from, _, (roundId, message)) => println(s"[${from.socket}][R$roundId][BYTE][${message.length}]: 0x${message.map("%02X" format _).mkString.take(40)}...[truncated]")
     case _ => println(event)
   }
 }
