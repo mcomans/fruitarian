ddabab44ca15b1b9dbe9b96d4c570985a7785f10
Nick Belzer <me@nickbelzer.me>
2020-03-31 17:59:19 +0200

    Update timeout logic
    This change makes the timeout logic store the roundId in which they
    started and compare it themselves to the current round id. The future
    now returns true if a timeout has occured and false otherwise. This
    allows the use of onComplete allowing us to properly start the next
    round again.
    
    The Thread.sleep calls are broken up in to chunks of 10ms which seems to
    prevent the main thread from locking up.
    
    With these changes I am able to make a complete run of at least 1000
    messages with no TIMEOUT when testing the ExperimentObserver with a
    clique size of 1 (only the center node that is also running the experiment).

--- src/main/scala/nl/tudelft/fruitarian/observers/TransmissionObserver.scala
+++ src/main/scala/nl/tudelft/fruitarian/observers/TransmissionObserver.scala
@@ -1,14 +1,14 @@
 package nl.tudelft.fruitarian.observers
 
-import java.util.concurrent.Executors
-
 import nl.tudelft.fruitarian.models.{DCnet, NetworkInfo}
+import nl.tudelft.fruitarian.p2p.messages._
 import nl.tudelft.fruitarian.p2p.{Address, TCPHandler}
-import nl.tudelft.fruitarian.p2p.messages.{FruitarianMessage, NextRoundMessage, ResultMessage, TextMessage, TransmitMessage, TransmitRequest}
 import nl.tudelft.fruitarian.patterns.Observer
 
 import scala.collection.mutable
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future, Promise}
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.util.control.Breaks._
 import scala.util.{Random, Try}
 
 /**
@@ -18,9 +18,8 @@ import scala.util.{Random, Try}
  * a message has been queued.
  */
 class TransmissionObserver(handler: TCPHandler, networkInfo: NetworkInfo) extends Observer[FruitarianMessage] {
-  protected implicit val context: ExecutionContextExecutorService =
-    ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(16))
-  var messageRound: Promise[Boolean] = _
+  protected implicit val context: ExecutionContextExecutor =
+    ExecutionContext.global
   val MESSAGE_ROUND_TIMEOUT = 5000
   val BACKOFF_RANGE = 10
   var messageQueue = new mutable.Queue[String]()
@@ -31,7 +30,8 @@ class TransmissionObserver(handler: TCPHandler, networkInfo: NetworkInfo) extend
   /**
    * Add a message to the message queue.
    * The message will be sent in the next round if the backoff is not set.
-   * @param message The message to be sent.
+   *
+   * @param message    The message to be sent.
    * @param prioritise Whether to prioritise this message (aka send it as
    *                   soon as possible instead of adding it in the back of
    *                   the queue).
@@ -65,41 +65,60 @@ class TransmissionObserver(handler: TCPHandler, networkInfo: NetworkInfo) extend
   def startMessageRound(): Unit = {
     // Clear possible remaining responses.
     DCnet.clearResponses()
-    messageRound = Promise[Boolean]()
+    roundId += 1
 
     // Send a TransmitRequest to all peers and itself (as this node is also part of the clique).
-    sendMessageToClique((address: Address) => TransmitRequest(networkInfo.ownAddress, address, roundId))
+    Await.ready(sendMessageToClique((address: Address) => TransmitRequest
+    (networkInfo.ownAddress, address, roundId)), 100.millis)
 
     // Set the amount of requests sent.
     DCnet.transmitRequestsSent = networkInfo.cliquePeers.length + 1
 
-    println(s"[S] [${networkInfo.nodeId}] Started Message round for ${DCnet.transmitRequestsSent} node(s).")
+    println(s"[S] [${networkInfo.nodeId}] Started Message round " +
+      s"[R$roundId] for ${DCnet.transmitRequestsSent} node(s).")
 
-    Future {
-      // TODO: Find a better way to sleep, this seems to cause the logs to be somewhat delayed.
-      Thread.sleep(MESSAGE_ROUND_TIMEOUT)
-      if (!messageRound.isCompleted) {
-        messageRound failure (_)
-        roundId += 1
-        println("[S] Message round timed out, retrying...")
-
-        // Send a "TIMEOUT" Text message to all peers to let them know the
-        // round failed and trigger the message requeue behaviour if one of
-        // them actually sent a message this round.
-        sendMessageToClique((address: Address) => ResultMessage(networkInfo.ownAddress, address, "TIMEOUT"))
-
-        // Give some additional time, and retry.
-        Thread.sleep(MESSAGE_ROUND_TIMEOUT)
-        startNextRound(roundId)
+    val timeoutFuture = Future[Boolean] {
+      // Save the roundId in which this timeout future started.
+      val startRound = s"$roundId".toInt;
+      breakable {
+        // Break the sleep in to steps of 10ms.
+        for (_ <- 1 to MESSAGE_ROUND_TIMEOUT/10) {
+          Thread.sleep(10)
+          // Each step compare the current roundId and break early in case
+          // the roundId has already gone up.
+          if (this.synchronized { roundId } > startRound) {
+            break
+          }
+        }
       }
+      // After the timeout, return true if the startRound is equal to the
+      // current round.
+      startRound == this.synchronized { roundId }
     }
-
+    timeoutFuture.onComplete((r: Try[Boolean]) => breakable {
+      // If the timeout was completed with false it means the round was
+      // completed successfully. If it completed with true it means we are
+      // stuck in the current round, thus trigger the timeout code below.
+      if (!r.get) {
+        break;
+      }
+      println(s"[S] Message round [$roundId] timed out, retrying...")
+      // Send a "TIMEOUT" Text message to all peers to let them know the
+      // round failed and trigger the message requeue behaviour if one of
+      // them actually sent a message this round.
+      Await.ready(sendMessageToClique((address: Address) => ResultMessage
+      (networkInfo.ownAddress, address, "TIMEOUT")), 100.millis)
+      // Give a little time for processing on the clients before starting the
+      // next round.
+      Thread.sleep(5)
+      startMessageRound()
+    })
   }
 
   /**
    * Helper function to send a message to all clique peers and itself.
    */
-  def sendMessageToClique(msg: (Address) => FruitarianMessage): Unit = {
+  def sendMessageToClique(msg: (Address) => FruitarianMessage): Future[Unit] = Future {
     networkInfo.cliquePeers.foreach(p => handler.sendMessage(msg(p.address)))
     handler.sendMessage(msg(networkInfo.ownAddress))
   }
@@ -115,9 +134,7 @@ class TransmissionObserver(handler: TCPHandler, networkInfo: NetworkInfo) extend
 
   override def receiveUpdate(event: FruitarianMessage): Unit = event match {
     case TransmitRequest(from, to, reqRoundId) =>
-      // Set the roundId to the highest value, as our random generators can only generate extra values.
-      roundId = math.max(roundId, reqRoundId)
-      if (messageQueue.nonEmpty && messageSent.isEmpty && backoff == 0 && roundId == reqRoundId) {
+      if (messageQueue.nonEmpty && messageSent.isEmpty && backoff == 0) {
         // If we have a message to send and are not waiting for confirmation
         // of a previous message, send the next message. If we failed to send
         // a message and have a backoff we have to wait this cycle.
